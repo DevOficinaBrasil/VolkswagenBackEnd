2ff1bebb5f50d87a2515f76955ef3ae1f33fe248
William Oliveira <will_belo1@hotmail.com>
2024-07-02 12:43:49 -0300

    Feat/ Atualização da presença do usuário

---
+++ app/Events/LogUser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\UserLog;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class LogUser
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($training, $user, $concessionaire)
+    {
+        $data = UserLog::firstOrCreate([
+            'CadastroID'        => $user,
+            'Treinamento'       => $training,
+            'concessionaire_id' => $concessionaire,
+        ],[
+            'TreinamentoParticipou' => 'S',
+            'Participou'            => 'P',
+        ]);
+        
+        if(!$data->wasRecentlyCreated){
+            $data->TreinamentoParticipou = $data->TreinamentoParticipou == 'S' ? 'N' : 'S';
+
+            $data->save();
+        }
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
